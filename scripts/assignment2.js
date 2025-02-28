2e02b2a8fa18de98a586bd932dc4b0dda74f260b
George14619 <george.robinson0930@gmail.com>
2025-02-27 19:52:53 -0500

    assignment2

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,4 +1,16 @@
 console.log("Convert Celsius to Fahrenheit");
-let temperatureInCelsius = 79;
-let temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32;
-console.log(temperatureInCelsius + temperatureInFahrenheit);
+
+function convertTemperature(){
+    console.log("I am a Function");
+    let celsius= prompt("Enter Temp into Celsius");
+    console.log(celsius);
+    let fahrenheit =(celsius * 9/5) + 32;
+    console.log(fahrenheit);
+
+    // show the console log with a better message
+    console.log(`${celsius} Celsius = ${fahrenheit} Fahrenheit`);
+    document.getElementById("result").innerHTML = `The conversion of Celsius ${celsius} to Farenheit is ${fahrenheit}`;
+}
+
+//let temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32;
+//console.log(temperatureInCelsius + temperatureInFahrenheit); document.getElementById("result").innerText = temperatureInCelsius + temperatureInFahrenheit;
