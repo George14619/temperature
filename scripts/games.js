50bb074963444a796d49ca94492fbc3b366daee2
George14619 <george.robinson0930@gmail.com>
2025-03-04 20:06:33 -0500

    updated

--- scripts/games.js
+++ scripts/games.js
@@ -1,59 +1,16 @@
-let positionX = 175; // Horizontal position
-let positionY = 175; // Vertical position
+let position = 175;
 let character = document.getElementById("character");
-
-
-function moveLeft() {
-    if (positionX > 0) {
-        positionX -= 10;
-        character.style.left = positionX + "px";
+function moveLeft(){
+    if(position>0){
+        position=position-10;
+        character.style.left=position+"px";
     }
-    console.log("X:", positionX, "Y:", positionY);
+    console.log(position);
 }
 
-function moveRight() {
-    let maxRight = window.innerWidth - character.offsetWidth;
-    if (positionX < maxRight) {
-        positionX += 10;
-        character.style.left = positionX + "px";
-    }
-    console.log("X:", positionX, "Y:", positionY);
-}
+function moveRight(){
 
-function moveUp() {
-    if (positionY > 0) {
-        positionY -= 10;
-        character.style.top = positionY + "px";
-    }
-    console.log("X:", positionX, "Y:", positionY);
 }
-
-function moveDown() {
-    let maxBottom = window.innerHeight - character.offsetHeight;
-    if (positionY < maxBottom) {
-        positionY += 10;
-        character.style.top = positionY + "px";
-    }
-    console.log("X:", positionX, "Y:", positionY);
-}
-
-// keyboard controls
-document.addEventListener("keydown", function(event) {
-    switch(event.key) {
-        case "ArrowLeft":
-            moveLeft();
-            break;
-        case "ArrowRight":
-            moveRight();
-            break;
-        case "ArrowUp":
-            moveUp();
-            break;
-        case "ArrowDown":
-            moveDown();
-            break;
-    }
-});
 
 
 
